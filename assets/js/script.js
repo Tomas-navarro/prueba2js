2a16a3b1f23b9dc5bb332dff7da6975db049e7b6
Tomas Navarro <tomas.navarro.m@outlook.com>
2024-11-27 20:31:55 -0300

    proyecto terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,47 @@
-let contadorPrimerSticker=0
-let contadorSegundoSticker=0
-let contadorTercerSticker=0
+
+
+// Primera pregunta
+const imagen = document.getElementById('primeraImagen')
+let flag = false
+imagen.addEventListener('click', () => {
+    if (!flag) {
+        imagen.style.border = "1px solid red";
+        flag = true;
+    } else {
+        imagen.style.border = "none"; 
+        flag = false; 
+    }
+});
+
+// Segunda pregunta
+const btn = document.getElementById('botonSegundaPregunta')
+const cantidadStickers = document.getElementById('cantidadSticker') 
+btn.addEventListener('click', () => {
+    let contadorPrimerSticker = document.getElementById('cantidadGoku')
+    let contadorSegundoSticker = document.getElementById('cantidadPicolo')
+    let contadorTercerSticker = document.getElementById('cantidadVegeta')
+    const sumaStickers = Number(contadorPrimerSticker.value) + Number(contadorSegundoSticker.value) + Number(contadorTercerSticker.value)
+    
+    if ( sumaStickers <= 10 ){
+        cantidadStickers.innerHTML= 'Llevas '+ sumaStickers.toString() +' Stickers';
+    }else{
+        cantidadStickers.innerHTML= 'Llevas demasiados stickers';
+    }
+})
+// Tercera pregunta
+const contraseñaDefault=911
+const contraseñaDefault2=714
+const btnTercera= document.getElementById('botonTerceraPregunta')
+const parrafoContraseña=document.getElementById('parrafoContraseña')
+btnTercera.addEventListener('click', ()=>{
+    let primerNumero = document.getElementById('primerNumero').value
+    let segundoNumero = document.getElementById('segundoNumero').value
+    let tercerNumero = document.getElementById('tercerNumero').value
+    if (primerNumero+segundoNumero+tercerNumero === contraseñaDefault.toString()){
+        parrafoContraseña.innerHTML= "password 1 correcto"
+    }else if(primerNumero+segundoNumero+tercerNumero === contraseñaDefault2.toString()){
+        parrafoContraseña.innerHTML= "password 2 correcto"
+    }else{
+        parrafoContraseña.innerHTML= "password incorrecto"
+    }
+})
